77c421b704368f855cb5dd0a436f55b998a3882d
Michael <michael@michael-VirtualBox.(none)>
2013-11-23 15:47:59 -0500

    Hook up the form.

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,11 +1,25 @@
 class PicsController < ApplicationController
 
+	def create
+		# Pic.create(:lesson => 1, ...)
+		@pic = Pic.create( pic_params )
 
-		def index
+		redirect_to pics_path
+	end
 
-			@pics = Pic.all
 
-		end
+	def new
+		@pic = Pic.new
+	end
+		
 
+	def index
+		@pics = Pic.all
+	end
+
+	private
+	def pic_params
+		params.require(:pic).permit(:lesson, :emotion, :learned)
+	end
 
 end
