d6651580a1ab38b3ecb1dc4141cda2844c2d2629
Michael <michael@michael-VirtualBox.(none)>
2013-11-24 01:17:21 -0500

    made the image a link

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,8 @@
 class PicsController < ApplicationController
+	
+	def show
+		@pic = Pic.find( params[:id] )
+	end
 
 	def create
 		# Pic.create(:lesson => 1, ...)
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,7 +1,8 @@
 class Pic < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
-	#requires that an entry exist in the lesson field and is less than 100
-	#error messa
+	#requires that an entry exist in all fields
+	#lesson field and is less than 100
+	#error message
 	validates :lesson, :presence => true, :numericality => {:less_than_or_equal_to => 100}
 	validates :learned, :presence => true
 	validates :emotion, :presence => true
