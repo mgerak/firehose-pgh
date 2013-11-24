cfb92e81aae9c6ff2645c3835387b7874db49097
Michael <michael@michael-VirtualBox.(none)>
2013-11-24 00:42:28 -0500

    add validations

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,4 +1,9 @@
 class Pic < ActiveRecord::Base
-
 	mount_uploader :image, ImageUploader
+	#requires that an entry exist in the lesson field and is less than 100
+	#error messa
+	validates :lesson, :presence => true, :numericality => {:less_than_or_equal_to => 100}
+	validates :learned, :presence => true
+	validates :emotion, :presence => true
+	validates :image, :presence => true
 end
